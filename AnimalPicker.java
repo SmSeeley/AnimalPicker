cfcb475cba99680b17b881979ceac0e93b9f88e3
SmSeeley <smseeley@quinnipiac.edu>
2025-02-24 14:05:10 -0500

    Added the ability for users to pick an animal dog or cat

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,8 +1,20 @@
+import java.util.*;
 
 public class AnimalPicker {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Animal Picker please select 'cat' or 'dog' ");
+        String choice = scanner.nextLine().toLowerCase();
 
+        if (choice.equals("dog")) {
+            System.out.println("Dog ASCII ART HERE");
+        }
+        else if (choice.equals("cat")) {
+            System.out.println("CAT ASCII ART HERE");
+        }
+        else {
+           System.out.println("Invalid choice"); 
+        }
     }
 }
