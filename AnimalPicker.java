c5f8310a9d4eb60e27a1ecf2e8d6f629dfe544f2
SmSeeley <smseeley@quinnipiac.edu>
2025-02-25 13:10:59 -0500

    Added the ability for users to enter 'fish' and have the fish ASCII art printed

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -7,16 +7,22 @@ public class AnimalPicker {
 
         while (true) {
 
-            System.out.println("Welcome to Animal Picker please select 'cat' or 'dog' ");
+            System.out.println("Welcome to Animal Picker please select 'cat' or 'dog' or 'fish' ");
             String choice = scanner.nextLine().toLowerCase();
 
             if (choice.equals("dog")) {
                 System.out.println("  __      _\no\'\')}____//\n `_/      )\n (_(_/-(_/");
                 break;
-            } else if (choice.equals("cat")) {
+            } 
+            else if (choice.equals("cat")) {
                 System.out.println("    fe/\\_____/\\\n   /  o   o  \\\n  ( ==  ^  == )\n   )         (\n  (           )\n ( (  )   (  ) )\n(__(__)___(__)__)");
                 break;
-            } else {
+            } 
+            else if (choice.equals("fish")) {
+                System.out.println(" o\no      ______/~/~/~/__           /((\n  o  // __            ====__    /_((\n o  //  @))       ))))      ===/__((\n    ))           )))))))        __((\n    \\\\     \\)     ))))    __===\\ _((\n     \\\\_______________====      \\_((\n                                 \\((");
+                break;
+            }
+            else {
                 System.out.println("Error");
                 System.out.println("Invalid choice");
             }
